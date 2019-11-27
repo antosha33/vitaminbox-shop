343fc07edb42110c182a797700e40989e1f56cf9
Anton Yegorov <an_tosha33@mail.ru>
2019-11-27 19:42:30 +0300

    feat: init project

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://vitaminbox/admin/');
+define('HTTP_CATALOG', 'http://vitaminbox/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://vitaminbox/admin/');
+define('HTTPS_CATALOG', 'http://vitaminbox/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/vitaminbox/admin/');
+define('DIR_SYSTEM', 'W:/domains/vitaminbox/system/');
+define('DIR_IMAGE', 'W:/domains/vitaminbox/image/');
+define('DIR_STORAGE', 'W:/domains/storage/');
+define('DIR_CATALOG', 'W:/domains/vitaminbox/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_vitaminbox', DIR_STORAGE . 'vitaminbox/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'vitaminbox');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ system/storage/cache/5e/5ee61a4e3848f5d58f731bcd1d5d5e00a5de9067e57b1504e5602d10eda5ac9c.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_dee4aa0585ed33f35ea32996c1ad73356d4d5690762ea073e86d672f85248b72 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
